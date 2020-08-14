1fa9d2b0ff70ee752171cbbe450563b449180853
Ryan Tharp <neuro@interx.net>
2020-08-13 22:56:24 -0700

    loadIdentity(), open() guard/lastHash & message emit, send() include displayNae

--- session-client.js
+++ session-client.js
@@ -2,30 +2,49 @@ const libsignal     = require('libsignal')
 const EventEmitter  = require('events')
 const lib = require('./lib/lib.js')
 
+const keyUtil = require('./external/mnemonic/index.js')
+
 class SessionClient extends EventEmitter {
   // options.seed
   // options.keypair
   // options.seedUrl
   constructor(options = {}) {
     super()
+    //console.log('ourPubkeyHex', this.ourPubkeyHex)
+    this.pollRate = options.pollRate || 1000
+    this.lastHash = options.lastHash || ''
+    this.displayName = false
+  }
+
+  // maybe a setName / setAvatar option
+  async loadIdentity(options = {}) {
     if (options.seed) {
       // decode seed into keypair
+      options.keypair = keyUtil.wordsToKeyPair(options.seed)
+      console.log('Loaded SessionID', options.keypair.pubKey.toString('hex'), 'from seed words')
     }
     // ensure keypair
     if (!options.keypair) {
-      options.keypair = libsignal.curve.generateKeyPair()
+      const res = await keyUtil.newKeypair()
+      console.log('SessionID', res.keypair.pubKey.toString('hex'), 'seed words:', res.words)
+      options.keypair = res.keypair
+    }
+    if (options.displayName) {
+      this.displayName = options.displayName
     }
     // process keypair
     this.keypair = options.keypair
     this.ourPubkeyHex = options.keypair.pubKey.toString('hex')
-    //console.log('ourPubkeyHex', this.ourPubkeyHex)
-    this.pollRate = options.pollRate || 1000
-    this.lastHash = options.lastHash || ''
   }
 
   async open() {
+    if (!this.ourPubkeyHex || this.ourPubkeyHex.length < 66) {
+      console.error('no identity loaded')
+      return
+    }
     if (!this.swarmUrl) {
       this.swarmUrl = await lib.getSwarmsnodeUrl(this.ourPubkeyHex)
+      //console.log('setting swarmUrl for', this.ourPubkeyHex, 'to', this.swarmUrl)
     }
     // lazy load recv library
     if (!this.recvLib) {
@@ -40,10 +59,15 @@ class SessionClient extends EventEmitter {
     const result = await this.recvLib.checkBox(
       this.ourPubkeyHex, this.swarmUrl, this.keypair, this.lastHash, lib
     )
-    this.lastHash = result.lastHash
-    if (result.messages.length) {
-      // emit them...
-      this.emit('messages', result.messages)
+    if (result) {
+      if (result.lastHash !== this.lastHash) {
+        this.emit('updateLastHash', result.lastHash)
+        this.lastHash = result.lastHash
+      }
+      if (result.messages.length) {
+        // emit them...
+        this.emit('messages', result.messages)
+      }
     }
     setTimeout(() => {
       if (this.open) {
@@ -56,12 +80,15 @@ class SessionClient extends EventEmitter {
     this.open = false
   }
 
+  // this.sendLib.send is async
   send(destination, messageTextBody, options = {}) {
     // lazy load recv library
     if (!this.sendLib) {
       this.sendLib = require('./lib/send.js')
     }
-    return this.sendLib.send(destination, this.keypair, messageTextBody, lib)
+    return this.sendLib.send(destination, this.keypair, messageTextBody, lib, {
+      displayName: this.displayName
+    })
   }
 }
 
