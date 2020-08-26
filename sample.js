9e594cf06e3b6c4c2710122a0c10f647678827dd
Ryan Tharp <neuro@interx.net>
2020-08-26 01:35:14 -0700

    include displayName/avatarFile

--- sample.js
+++ sample.js
@@ -1,10 +1,30 @@
+const fs = require('fs')
 const SessionClient = require('./session-client.js')
 
 const client = new SessionClient()
+client.loadIdentity({
+  seed: fs.existsSync('seed.txt') && fs.readFileSync('seed.txt').toString(),
+  //displayName: 'This is my name',
+  //avatarFile: '5f16f4fd7ca8def05968bbca_Jk79urotkJJtMHZNO3kduoJLgAW6X6kgceEjnbI2VeeOseBujKs6ok_IbYl3OHxaaHLUmtMVRNk.png',
+}).then(async () => {
+  client.open()
+  client.on('messages', msgs => {
+    msgs.forEach(async msg => {
+      console.log('msg', msg)
+      /*
+      if (msg.attachments.length) {
+        const attachments = await client.getAttachments(msg)
+        console.log('attachments', attachment)
+      }
+      */
+    })
+  })
+  const toPubkey = '05d233c6c8daed63a48dfc872a6602512fd5a18fc764a6d75a08b9b25e7562851a'
 
-client.open()
-client.on('messages', msgs => {
-  console.log('newMessages', msgs)
+  // need an image
+  //const attachment = await client.makeAttachment(fs.readFileSync('/Users/user2/Pictures/1587699732-0s.png'))
+  // console.log('attachment', attachment)
+  client.send(toPubkey, 'Hello', {
+    // attachments: [attachment]
+  })
 })
-const toPubkey = '05d233c6c8daed63a48dfc872a6602512fd5a18fc764a6d75a08b9b25e7562851a'
-client.send(toPubkey, 'Hello')
