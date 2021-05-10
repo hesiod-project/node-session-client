1bfa53098efa8f2b01664f3426fa9a00c1681c48
Ryan Tharp <neuro@interx.net>
2021-05-10 03:39:35 -0700

    comment out sogs, add sogsv2 example

--- sample.js
+++ sample.js
@@ -28,28 +28,38 @@ client.loadIdentity({
   })
 
   //const openGroupName = 'session.lokisn.com'
-  const openGroupName = 'feedback.getsession.org'
+  //const openGroupName = 'feedback.getsession.org'
+  const openGroupV2URL = 'http://open2.hesiod.network/build_a_bot?public_key=58dc124cc38e4d03449037e9a4a86a2e5c2a648938eb824a5cdf3b6a80fab07d'
 
-  const ogOpened = await client.joinOpenGroup(openGroupName)
+  //const ogOpened = await client.joinOpenGroup(openGroupName)
+  const ogv2Handle = await client.joinOpenGroupV2(openGroupV2URL)
 
   // handle incoming messages
   client.on('messages', msgs => {
     msgs.forEach(async msg => {
       console.log()
-      console.log(`New message, ${msg.openGroup ? `In group: ${msg.openGroup}` : 'Private'}:`)
-      console.log(`From: ${msg.profile.displayName} (${msg.source})`)
+      /*
+      if (msg.openGroup) {
+        console.log(`New message, In group: ${msg.openGroup}`)
+      } else */
+      if (msg.openGroupV2) {
+        console.log(`New message, In group V2: ${msg.openGroupV2}`)
+      } else {
+        console.log('New message, Private')
+      }
+      console.log(`From: ${msg.profile && msg.profile.displayName} (${msg.source})`)
       console.log(msg.body)
 
       // Attachment processing example
-      /*
-      if (msg.attachments.length) {
-        const attachments = await client.getAttachments(msg)
-        //console.log('attachment', attachments[0])
-        fs.writeFileSync('imagetest.png', attachments[0])
+      if (0) {
+        if (msg.attachments.length) {
+          const attachments = await client.getAttachments(msg)
+          //console.log('attachment', attachments[0])
+          fs.writeFileSync('imagetest.png', attachments[0])
+        }
       }
-      */
-      // Open group invitation example
       /*
+      // Open group invitation example
       if (msg.groupInvitation) {
         console.log('got invite to channel', msg.groupInvitation.channelId)
       }
@@ -60,7 +70,7 @@ client.loadIdentity({
   await client.open()
 
   // TODO: replace with your SessionID
-  const SessionID = '05ce7f197b3e4fa80331397ebb68fb5d66e47f92594ab76bb67233105b78ced238' // ipadtop
+  const SessionID = ''
 
   // LNS example
   //
@@ -72,17 +82,25 @@ client.loadIdentity({
   //
   // need an image
   //const attachment = await client.makeImageAttachment(fs.readFileSync('/Users/user2/Pictures/1587699732-0s.png'))
-  client.send(SessionID, 'Hello', {
-    // attachments: [attachment]
-  }).then(() => {
-    console.debug('Sent "Hello" to', SessionID)
-  })
-
-  if (ogOpened) {
-    //await client.deleteOpenGroupMessage(openGroupName, [28, 27, 26])
+  if (0) {
+    client.send(SessionID, 'Hello', {
+      // attachments: [attachment]
+    }).then(() => {
+      console.debug('Sent "Hello" to', SessionID)
+    })
   }
 
-  //await client.sendOpenGroupMessage(openGroupName, 'Sample Open Group Message 1')
+  /*
+  if (ogOpened) {
+    const messageId = await client.sendOpenGroupMessage(openGroupName, 'Sample Open Group Message 1')
+    await client.deleteOpenGroupMessage(openGroupName, [messageId])
+  }
+  */
+
+  if (0 && ogv2Handle) {
+    const messageId = await client.sendOpenGroupV2Message(ogv2Handle, 'Hello World SOGSv2 from node-session-client')
+    await client.deleteOpenGroupV2Message(ogv2Handle, messageId)
+  }
 
   // Open group invite example
   // only works on desktop
